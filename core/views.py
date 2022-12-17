ed2f264a6769d9ca4156ccfbe744611ca22b20e3
bpgrabi <bpater@student.agh.edu.pl>
2022-12-17 23:52:28 +0100

    return list of proposals

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import SearchForm
-from core.models import Product
+from ceneoscraper import bs4_scraper as scraper
 
 
 def home(request):
@@ -12,11 +12,10 @@ def search(request):
     if request.method == "POST":
         item_to_search = request.POST.get("search_item")
         allegro_option = request.POST.get("options")
-        context = {
-            "name": item_to_search,
-            "option": allegro_option
-        }
-        return render(request, "base.html", {"context": context})
+        context = {"name": item_to_search, "option": allegro_option}
+        ready_link = scraper.prepare_link(context.get("name"), "")
+        proposals = scraper.get_products(ready_link)
+        return render(request, "base.html", {"proposals": proposals})
     else:
         form = SearchForm()
         context = {"form": form}
