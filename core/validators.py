28fe75999846ff1b80dc4a6c79cf94eb6bd7b787
Piotr Wodecki <piotr.m.wodecki@gmail.com>
2023-01-14 15:16:17 +0100

    Reformat validators

--- core/validators.py
+++ core/validators.py
@@ -1,13 +1,11 @@
-# place for every validators
+import os
+from django.core.exceptions import ValidationError
 
 
 def validate_file_extension(value):
-    import os
-    from django.core.exceptions import ValidationError
-
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = [".txt", ".csv"]
-    if not ext.lower() in valid_extensions:
+    if ext.lower() not in valid_extensions:
         raise ValidationError("Unsupported file extension.")
 
 
@@ -16,13 +14,13 @@ def validate_multi_search_files_row(row):
         if len(str(row[0])) > 32:
             # name of product can not be longer than 32
             return False
-        if not row[1] in ["1", "2", "3"]:
+        if row[1] not in ["1", "2", "3"]:
             # there are three types of shopping (All, only allegro, without allegro)
             return False
-        if not row[2] in ["Zdrowie", "Uroda"]:
+        if row[2] not in ["Zdrowie", "Uroda"]:
             # there are two types of category
             return False
-        if not int(row[3]) > 0:
+        if int(row[3]) <= 0:
             # quantity can not be less than 1
             return False
         return True
